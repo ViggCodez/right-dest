65a8559e8a21e892204c82f53510b107d90888fe
Viggcodez <vigneshbaiju.vb@gmail.com>
2024-07-10 18:02:21 +0530

    updated index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './component/Navbar';
+import Navbar from './Components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
